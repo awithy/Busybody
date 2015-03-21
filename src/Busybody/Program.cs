225c51132ca9374b522fed2136c2a1780f706553
Adrian <Adrian>
2015-03-20 19:35:51 -0700

    Seutp TopShelf service host

--- src/Busybody/Program.cs
+++ src/Busybody/Program.cs
@@ -1,54 +1,71 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
-using System.Threading;
 using Busybody.Config;
+using Topshelf;
 
 namespace Busybody
 {
     class Program
     {
+        static Logger _log;
+        static bool _debug;
+        static Host _serviceHost;
+
         static int Main(string[] args)
         {
             //TODO: Handle unhandled exceptions
 
-            if (args.Contains("-d") || args.Contains("--debug"))
-                LogSetup.EnableConsoleDebug();
+            var host = _ConfigureServiceHost();
 
             var busybodyTempPath = CommonPaths.BusybodyTemp();
             Directory.CreateDirectory(busybodyTempPath);
             _SetupLogging();
+            if (_debug)
+                LogSetup.EnableConsoleDebug();
 
-            var log = new Logger(typeof (Program));
+            _log = new Logger(typeof (Program));
             try
             {
-                log.Info("Starting Busybody v0.1");
+                _log.Info("Starting Busybody v0.1");
 
                 AppContext.Instance = new AppContext();
                 var configFilePath = CommonPaths.CurrentConfigFilePath();
                 AppContext.Instance.Config = new ConfigFileReader().ReadFromFile(configFilePath);
-                var busybodyDaemon = new BusybodyDaemon();
-                busybodyDaemon.Start();
 
-                log.Info("Startup complete");
+                _log.Debug("Running host");
+                host.Run();
 
-                while(true)
-                {
-                    var key = Console.ReadKey();
-                    if (key.Key == ConsoleKey.Q)
-                    { 
-                        busybodyDaemon.Stop();
-                        return 0;
-                    }
-
-                    Thread.Sleep(1000);
-                }
+                return 0;
             }
             catch (Exception ex)
             {
-                log.Error("Unexpected " + ex.GetType().Name + " occurred.  Aborting.  " + Environment.NewLine + ex);
+                _log.Error("Unexpected " + ex.GetType().Name + " occurred.  Aborting.  " + Environment.NewLine + ex);
                 return -1;
             }
+        }
+
+        static Host _ConfigureServiceHost()
+        {
+            var serviceHost = HostFactory.New(x =>
+            {
+                x.AfterInstall(() => _log.Info("The Busybody service has been installed."));
+                x.BeforeUninstall(() => _log.Info("The Busybody service is being uninstalled."));
+                x.RunAsLocalSystem();
+                x.SetDescription("Busybody");
+                x.SetDisplayName("Busybody");
+                x.SetServiceName("Busybody");
+                x.AddCommandLineSwitch("d", debug => _debug = debug);
+
+                x.Service<BusybodyDaemon>(s =>
+                {
+                    x.SetServiceName("Busybody");
+                    s.ConstructUsing(name => new BusybodyDaemon());
+                    s.WhenStarted(busybodyDaemon => busybodyDaemon.Start());
+                    s.WhenStopped(busybodyDaemon => busybodyDaemon.Stop());
+                });
+
+            });
+            return serviceHost;
         }
 
         static void _SetupLogging()
