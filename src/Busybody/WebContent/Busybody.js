399d6e9a81bdc512c712a5e678fdc6adcc4f650e
Adrian <Adrian>
2015-04-15 21:49:03 -0700

    Auto refresh web pages

--- src/Busybody/WebContent/Busybody.js
+++ src/Busybody/WebContent/Busybody.js
@@ -1,17 +1,25 @@
 ﻿var app = angular.module('busybodyApp', []);
 
-app.controller('hostsController', function($scope, $http) {
-    $http.get("/hosts")
-    .success(function (response) {
-        $scope.hosts = response;
-    });
+app.controller('hostsController', function($scope, $http, $interval) {
+    var update = function () {
+        $http.get("/hosts")
+            .success(function(response) {
+                $scope.hosts = response;
+            });
+    };
+    $interval(update, 1000);
+    update();
 });
 
-app.controller('eventLogApiController', function($scope, $http) {
-    $http.get("/eventLogApi")
-    .success(function (response) {
-        $scope.events = response;
-    });
+app.controller('eventLogApiController', function($scope, $http, $interval) {
+    var update = function () {
+        $http.get("/eventLogApi")
+            .success(function(response) {
+                $scope.events = response;
+            });
+    };
+    $interval(update, 1000);
+    update();
 });
 
 app.filter('fromNow', function() {
