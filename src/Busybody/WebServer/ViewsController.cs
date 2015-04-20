59d6d495fa0c07111196881be6ad79e00074fc5c
Adrian <Adrian>
2015-04-19 20:48:11 -0700

    Views controller

---
+++ src/Busybody/WebServer/ViewsController.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Busybody.WebServer
+{
+    [Authorize]
+    public class ViewsController : ApiController
+    {
+        public HttpResponseMessage GetViews()
+        {
+            return new HttpResponseMessage(HttpStatusCode.NotFound);
+        }
+
+        public HttpResponseMessage GetViewById(string id)
+        {
+            return WebServerHelpers.GetView(id);
+        }
+    }
+}
