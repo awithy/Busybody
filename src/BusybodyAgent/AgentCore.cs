0f4414fa804d8195143730e2fe2554be98790aed
Adrian <Adrian>
2015-05-16 13:07:13 -0700

    Created empty agent core

---
+++ src/BusybodyAgent/AgentCore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BusybodyAgent
+{
+    public class AgentCore
+    {
+        public void Poll(DateTime timestamp)
+        {
+            //var filePath = Path.Combine(CommonPaths.BusybodyData(), "AgentStatus.txt");
+            //File.WriteAllText(filePath, timestamp.ToString("o"));
+        }
+    }
+}
--- src/BusybodyAgent/BusybodyAgentDaemon.cs
+++ src/BusybodyAgent/BusybodyAgentDaemon.cs
@@ -1,15 +1,47 @@
-﻿namespace BusybodyAgent
+﻿using System;
+using System.Threading;
+
+namespace BusybodyAgent
 {
     public class BusybodyAgentDaemon
     {
+        AgentCoreRoleService _agentCoreRoleService;
+
         public void Start()
         {
-            
+            _agentCoreRoleService = new AgentCoreRoleService();
+            _agentCoreRoleService.Start();
         }
 
         public void Stop()
         {
-            
+            _agentCoreRoleService.Stop();
+        }
+    }
+
+    public class AgentCoreRoleService : AgentRoleServiceBase
+    {
+        AgentCore _agentCore;
+
+        public override string Name
+        {
+            get { return "Agent Core"; }
+        }
+
+        public override TimeSpan Period
+        {
+            get { return TimeSpan.FromSeconds(AppContext.Instance.Config.PollingInterval); }
+        }
+
+        protected override void _Initialize(DateTime time)
+        {
+            _agentCore = new AgentCore();
+            base._Initialize(time);
+        }
+
+        protected override void _OnPoll(CancellationToken cancellationToken)
+        {
+            _agentCore.Poll(DateTime.UtcNow);
         }
     }
 }
