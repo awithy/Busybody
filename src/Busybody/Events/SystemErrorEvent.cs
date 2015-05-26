c295b2f8c7effc8d2145230b802399ed98c64c03
Adrian <Adrian>
2015-05-25 17:54:29 -0700

    return message on system error event log string

--- src/Busybody/Events/SystemErrorEvent.cs
+++ src/Busybody/Events/SystemErrorEvent.cs
@@ -10,5 +10,10 @@ namespace Busybody.Events
             Message = message;
             Detail = detail;
         }
+
+        public override string ToLogString()
+        {
+            return Message;
+        }
     }
 }
